2bbda607d747a0f77425e5c798eec7aca91d55a5
967abhi <abhisheksinghbhtr967@gmail.com>
2024-02-19 15:39:38 +0530

    first commit

---
+++ src/constants/index.js
@@ -0,0 +1,202 @@
+import { meta, shopify, starbucks, tesla } from "../assets/images";
+import {
+  car,
+  contact,
+  css,
+  estate,
+  express,
+  git,
+  github,
+  html,
+  javascript,
+  linkedin,
+  mongodb,
+  motion,
+  mui,
+  nextjs,
+  nodejs,
+  pricewise,
+  react,
+  redux,
+  sass,
+  snapgram,
+  summiz,
+  tailwindcss,
+  threads,
+  typescript,
+} from "../assets/icons";
+
+export const skills = [
+  {
+    imageUrl: css,
+    name: "CSS",
+    type: "Frontend",
+  },
+  {
+    imageUrl: express,
+    name: "Express",
+    type: "Backend",
+  },
+  {
+    imageUrl: git,
+    name: "Git",
+    type: "Version Control",
+  },
+  {
+    imageUrl: github,
+    name: "GitHub",
+    type: "Version Control",
+  },
+  {
+    imageUrl: html,
+    name: "HTML",
+    type: "Frontend",
+  },
+  {
+    imageUrl: javascript,
+    name: "JavaScript",
+    type: "Frontend",
+  },
+  {
+    imageUrl: mongodb,
+    name: "MongoDB",
+    type: "Database",
+  },
+  {
+    imageUrl: motion,
+    name: "Motion",
+    type: "Animation",
+  },
+  {
+    imageUrl: mui,
+    name: "Material-UI",
+    type: "Frontend",
+  },
+  {
+    imageUrl: nextjs,
+    name: "Next.js",
+    type: "Frontend",
+  },
+  {
+    imageUrl: nodejs,
+    name: "Node.js",
+    type: "Backend",
+  },
+  {
+    imageUrl: react,
+    name: "React",
+    type: "Frontend",
+  },
+  {
+    imageUrl: redux,
+    name: "Redux",
+    type: "State Management",
+  },
+  {
+    imageUrl: sass,
+    name: "Sass",
+    type: "Frontend",
+  },
+  {
+    imageUrl: tailwindcss,
+    name: "Tailwind CSS",
+    type: "Frontend",
+  },
+  {
+    imageUrl: typescript,
+    name: "TypeScript",
+    type: "Frontend",
+  },
+];
+
+export const experiences = [
+  {
+    title: "Web Developer",
+    company_name: "LetsGrowMore",
+    icon: starbucks,
+    iconBg: "#accbe1",
+    date: "Sep 2021 - Oct 2021",
+    points: [
+      "Developing the website the Movie website and the Music website ",
+      "The tech stack used for this internship is HTML,CSS AND JAVASCRIPT",
+      "Implementing responsive design and ensuring cross-browser compatibility.",
+      "Participating in code reviews and providing constructive feedback to other developers.",
+    ],
+  },
+  {
+    title: "Web Developer",
+    company_name: "Suven Consultants and ",
+    icon: tesla,
+    iconBg: "#fbc3bc",
+    date: "Dec 2021-Jan 2021",
+    points: [
+      "Developing and maintaining web applications using React.js and other related technologies.",
+      "Implementing responsive design and ensuring cross-browser compatibility.",
+    ],
+  },
+  {
+    title: "Junior Software Engineer ",
+    company_name: "Ripple Healthcare Private Limited ",
+    icon: shopify,
+    iconBg: "#b7e4c7",
+    date: "Aug 2023 - Present",
+    points: [
+      "Developing and maintaining web applications using React.js and other related technologies.",
+      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+      "Implementing responsive design and ensuring cross-browser compatibility.",
+    ],
+  },
+];
+
+export const socialLinks = [
+  {
+    name: "Contact",
+    iconUrl: contact,
+    link: "/contact",
+  },
+  {
+    name: "GitHub",
+    iconUrl: github,
+    link: "https://github.com/967abhi",
+  },
+  {
+    name: "LinkedIn",
+    iconUrl: linkedin,
+    link: "https://www.linkedin.com/in/abhishek-kumar-singh-852353204/",
+  },
+];
+
+export const projects = [
+  {
+    iconUrl: pricewise,
+    theme: "btn-back-red",
+    name: "Movie Streaming Website ",
+    description:
+      "Developed a Netflix clone  website utilizing Reactjs, React-Redux, and Firebase..",
+    link: "https://www.youtube.com/watch?v=MvMNQcTSHFE",
+  },
+  {
+    iconUrl: threads,
+    theme: "btn-back-green",
+    name: "Gossip App ",
+    description:
+      "Created a messenger application by using Re-act and Socket.io.The application can display the user joining and leaving a message.",
+    link: "https://www.youtube.com/watch?v=wwWgLWeFyn4",
+  },
+  {
+    iconUrl: car,
+    theme: "btn-back-blue",
+    name: "Event Organiser Website ",
+    description:
+      "Developed a multi-page event organizer website using HTML, CSS, JavaScript, Node.js, and MongoDB.",
+    link: "https://967abhi.github.io/task1fouses/",
+  },
+  {
+    iconUrl: motion,
+    theme: "btn-back-green",
+    name: "Cooking App",
+    description:
+      "This is the Recipe Website which is created using Nodejs and Mongodb and Ejs . In this website you can search the product .you are able to add the recipe delete the recipe ",
+    link: "https://youtu.be/o0XsCMxOKUQ",
+  },
+];
